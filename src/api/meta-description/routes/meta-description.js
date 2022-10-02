ace609c317a3febf590d51257b4a9519123a0ab8
farouqdaif1 <daiffarouq@gmail.com>
2022-10-02 20:16:01 +0200

    Add meta descraption and Key words

---
+++ src/api/meta-description/routes/meta-description.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * meta-description router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::meta-description.meta-description');
---
+++ src/api/meta-description/services/meta-description.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * meta-description service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::meta-description.meta-description');
